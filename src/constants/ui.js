061443f78759e0147c51ff4f6a99a4284f55d0d8
Rui Rocha <rui.rocha.work@outlook.com>
2026-04-05 22:56:50 +0100

    added basin url for no backend form

--- src/constants/ui.js
+++ src/constants/ui.js
@@ -1,5 +1,5 @@
 // UI constants
 export const SESSION_KEY = 'arg_insights_seen';
 export const ALREADY_SUBSCRIBED_KEY = 'arg_insights_subscribed';
-export const BASIN_ENDPOINT = 'https://usebasin.com/f/df689d2bdfd6';
+export const BASIN_ENDPOINT = 'https://usebasin.com/f/61c6847a0a35';
 export const STORAGE_KEY = 'arg_cookie_consent';
